ccea54c9582d144ac6a2acf77afac4d81e1f1fc4
TSchotter <schotter.terratoast@gmail.com>
2026-01-28 12:56:29 -0500

    Wed, Jan 28th update

--- Jan23/BinaryTree.py
+++ Jan23/BinaryTree.py
@@ -21,7 +21,44 @@ class BinaryTree:
         self.head = None
 
     def add(self, data):
-        
+        # if it's not in the tree, we will add.
+
+        newNode = Node(data)
+        if (self.head == None):
+            # tree doesn't have a head node
+            self.head = newNode
+            return # done, time to leave
+
+        # traverse the tree until we find the correct spot
+        curNode = self.head # set pointer to look at start
+    
+
+        # loop until we find spot. 
+        # we will want the loop to end when we insert the new node
+        # or, when we run into the same data that we want to add
+
+        # if newNode data is the same as the data of curNode
+        #  leave loop
+        # curNode.data == newNode.data <- condition
+
+        # do I want to look left, or right.
+        # thing to add < what we're looking at
+        #  newNode.data < curNode.data
+
+        # is there something there in that direction?
+        #   left side ver.
+        #  curNode.left is None, or !None
+
+        # if there is nothing there, place the node
+        # curNode.left = newNode
+        # we're done, leave the loop
+
+        # if there IS something there,
+        # traverse left.
+        # move the pointer to the left
+        # curNode = curNode.left
+        # start the loop again
+
         pass # used as a placehold so you can have blank function
 
     def inorder_print(self, node):
