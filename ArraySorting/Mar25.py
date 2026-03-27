1f2add9ac9c7afc6ae20f569ff862cd15660f452
TSchotter <schotter.terratoast@gmail.com>
2026-03-27 12:38:36 -0400

    Updated Mar27

--- ArraySorting/Mar25.py
+++ ArraySorting/Mar25.py
@@ -24,24 +24,77 @@ def selectionSort(x):
         x[i], x[smallest] = x[smallest], x[i]
 
 def mergeSort(x):
-    
+    # kick off the recursion
+    split(x, 0, len(x)-1) # give it the full bounds of the list
 
 def split(x, start, end):
-    if end-start <= 1:
+    if (end-start) <= 0: # changed to <= 0, since <= 1 allows size 2 arrays to return early
         return
-    
-    middleIndex = (end-start)//2
+
+    middleIndex = (end-start)//2 + start
     split(x, start, middleIndex) # left side
     split(x, middleIndex+1, end) # right
 
     merge(x, start, middleIndex, end)
 
 def merge(x, start, middle, end):
-    left = x[start:middle+1]
+    left = x[start:middle+1]  # makes memory complexity O(n)
 
     i = start # the next spot we fill
     l = 0  # looking at start of left array
-    r = middle+1. # looking at start of right array
+    r = middle+1 # looking at start of right array
+
+    while(i <= end): # continue as long as we have spots to fill
+        # which value, x[l] or x[r] goes into x[i]
+        if (l < len(left) and r <= end): # both l and r in in bounds
+            if (x[r] < left[l]):
+                x[i] = x[r] # number at "r" was the smaller
+                r+=1 # look at next spot
+            else:
+                x[i] = left[l] # number at "l" was the smaller
+                l+=1
+        elif(l < len(left)): # r is out of bounds, only grab l
+            x[i] = left[l]
+            l+=1
+        else:
+            x[i] = x[r]
+            r+=1 # look at next spot
+        i += 1 # set target location to next spot
+
+
+def quickSort(x):
+    pass
+
+def naivePartition(x, start, end):
+    # the list is already "partitioned"
+    if end <= start:
+        return
+
+    pivot = x[end]
+    # create empty list that we fill.
+    copy = []
+    # find things less than pivot
+    for i in range(start, end):
+        # anything less than pivot, add it to "copy"
+        if x[i] <= pivot:
+            copy.append(x[i])
+
+    partitionIndex = start + len(copy)        
+    copy.append(pivot) # add the pivot
+
+    # find things greater than pivot
+    for i in range(start, end):
+        # anything greater than pivot, add it to "copy"
+        if x[i] > pivot:
+            copy.append(x[i])
+    
+    # place all things in copy into same spot on x
+    for i in range(len(copy)):
+        x[start+i] = copy[i]
+
+    # partition left and right
+    naivePartition(x, start, partitionIndex-1)
+    naivePartition(x, partitionIndex+1, end)
 
 
 random.seed(0)
@@ -54,7 +107,7 @@ for i in range(n):
 
 
 start = time.time()
-selectionSort(x)
-print(x[100:120])
+mergeSort(x)
+print(x[100:110])
 end = time.time()
 print(end-start) # number of seconds difference between two
